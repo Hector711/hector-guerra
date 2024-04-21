3c18d1c5eda84a23234bee2127336be9f887129c
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-04-21 04:11:16 +0200

    feat: :building_construction: Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import profile_photo from "@/assets/profile_photo.png";
 
 export default function Hero() {
   return (
     <header>
-      <div id='img'>
-        <h1>Héctor Guerra</h1>
-        <h2>Full Stack Web Developer</h2>
+      <div id="img">
+        <img src={profile_photo} alt="" />
+        <div id="header-container">
+          <h1>Héctor Guerra</h1>
+          <h2>Full Stack Developer</h2>
+        </div>
       </div>
-      <div id='about'>
+      <div id="about">
         <h2>About</h2>
         <p>
-          Enthusiastic beginner developer eager to break into the tech industry. Seeking first job opportunity to kickstart a fulfilling programming
+          Enthusiastic beginner developer eager to break into the tech industry.
+          Seeking first job opportunity to kickstart a fulfilling programming
           career.
         </p>
       </div>
-      <div id='location'>
+      <div id="location">
         <h2>Madrid, Spain.</h2>
       </div>
-      <div id='experience'>
+      <div id="experience">
         <h2>Experience</h2>
       </div>
     </header>
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,13 +1,66 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import Mail from "../icons/Mail.svg";
+import Whatsapp from "@/icons/Whatsapp.svg";
+import X from "@/icons/X.svg";
+import GitHub from "@/icons/GitHub.svg";
+import Instagram from "@/icons/Instagram.svg";
+import LinkedIn from "@/icons/LinkedIn.svg";
+import LocationIcon from "@/icons/LocationIcon.svg";
+
 export default function Navbar() {
   return (
     <nav>
       <div id="nav-container">
-        <NavLink><h3>Projects</h3></NavLink>
-        <NavLink><h3>Experience</h3></NavLink>
-        <NavLink><h3>Blog</h3></NavLink>
+        <a
+          href=''
+          title={`Visitar el perfil de  en `}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Mail} alt="" />
+        </a>
+        <a
+          href=''
+          title={`Visitar el perfil de  en `}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Whatsapp} alt="" />
+        </a>
+        <a
+          href=''
+          title={`Visitar el perfil de  en `}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={X} alt="" />
+        </a>
+        <a
+          href=''
+          title={`Visitar el perfil de  en `}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={GitHub} alt="" />
+        </a>
+        <a
+          href=''
+          title={`Visitar el perfil de  en `}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={LinkedIn} alt="" />
+        </a>
+        <a
+          href=''
+          title={`Visitar el perfil de  en `}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Instagram} alt="" />
+        </a>
       </div>
     </nav>
   );
