5413f6778ae2971a3a74d8e02e8ba6e0d8bec841
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-04-21 18:45:02 +0200

    feat:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import Section from "@/Sections/components/Section";
 import Hero from "@/Sections/Hero";
 import Projects from "@/Sections/Projects";
 import WorkExperience from "@/Sections/WorkExperience";
-import VideoResume from "../Sections/VideoResume";
+import VideoResume from "@/Sections/VideoResume";
 
 export default function Home() {
   return (
