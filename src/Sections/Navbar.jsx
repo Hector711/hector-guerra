365114bcd0467a91208236627f99f9cd47c94529
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-04-22 00:19:51 +0200

    feat:

--- src/Sections/Navbar.jsx
+++ src/Sections/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
+import DarkMode from "@/assets/DarkMode.svg";
+import Language from "@/assets/Language.svg";
 
 export default function Navbar() {
   return (
     <nav>
-      <div id="nav-container">
-        <button><a href="https://hector-minimalist-cv.netlify.app" target="_blank">Minimal CV</a></button>
-        <button>Language</button>
-        <button>Dark Mode</button>
+      <div id="nav-container" className="flex justify-between">
+        <div className="flex justify-center items-center">
+          <button>
+            <a href="https://hector-minimalist-cv.netlify.app" target="_blank">
+              Minimal CV
+            </a>
+          </button>
+        </div>
+        <div className="flex justify-center items-center gap-4 ">
+          <button>
+            <img src={Language} alt="" />
+          </button>
+          <button>
+            <img src={DarkMode} alt="" />
+          </button>
+        </div>
       </div>
     </nav>
   );
--- src/Sections/VideoResume.jsx
+++ src/Sections/VideoResume.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import ArrowRight from "@/assets/ArrowRight.svg"
 
 export default function VideoResume() {
   return (
-    <div className="flex justify-center mt-7 mb-3">
+    <div className="flex justify-center mt-7 mb-3 gap-4">
       <p className=" text-center">
         Tired of watching tons of portfolios?
         <br />
         Let me show you what you need to know about me in a 1 minute video!
       </p>
+      <img src={ArrowRight} alt="" />
+      <button>Video</button>
     </div>
   );
 }
