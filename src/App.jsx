364ad5cc94e43d220699c194c92c4aed4360cf5e
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-04-23 00:09:15 +0200

    fix:

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
-import Navbar from './sections/Navbar.jsx';
-import Footer from './sections/Footer';
+import Navbar from '@/sections/Navbar';
+import Footer from '@/sections/Footer';
 
 export default function App() {
   return (
