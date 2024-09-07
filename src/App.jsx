4cc97e6bba34727fe812b0076ca2ef68b196ec07
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-09-08 00:49:23 +0200

    fix: :rocket:

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import Navbar from './sections/Navbar.jsx';
-import Footer from './sections/Footer.jsx';
+import Navbar from './section/Navbar.jsx';
+import Footer from './section/Footer.jsx';
 import { PortfolioProvider } from './context/PortfolioContext.jsx';
 
 export default function App() {
