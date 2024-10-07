6519cea8ef0feb9c562b10c5b7bff1d2c4852332
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-10-07 22:10:23 +0200

    feat: Equipo y accordion FAQ

--- src/app/home/sections/FAQ.jsx
+++ src/app/home/sections/FAQ.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Section from '@/components/Section';
+import Accordion from '@/components/Accordion';
 
 export default function FAQ() {
   return (
     <Section title='Preguntas Frecuentes' id='faq'>
       <div>FAQ</div>
+      <Accordion />
     </Section>
   );
 }
