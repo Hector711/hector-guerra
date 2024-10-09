cdd75326024c6a8f93fc5f20033723ba77ce4ee0
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-10-09 13:56:27 +0200

    refactor: :art:

--- src/app/home/HomePage.jsx
+++ src/app/home/HomePage.jsx
@@ -1,22 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Hero from '@/app/home/sections/Hero';
-import { useTranslation } from 'react-i18next';
 import { Toaster } from 'react-hot-toast';
 import Claves from '@/app/home/sections/Claves';
 import Productos from '@/app/home/sections/Productos';
-import Herramientas from '@/app/home/sections/Herramientas';
+// import Herramientas from '@/app/home/sections/Herramientas';
 import Interseccion1 from '@/app/home/sections/Interseccion1';
 import Interseccion2 from '@/app/home/sections/Interseccion2';
 import Interseccion3 from '@/app/home/sections/Interseccion3';
 import Interseccion4 from '@/app/home/sections/Interseccion4';
-import Interseccion5 from '@/app/home/sections/Interseccion5';
+// import Interseccion5 from '@/app/home/sections/Interseccion5';
 import Testimonios from '@/app/home/sections/Testimonios';
 import Precios from '@/app/home/sections/Precios';
 import Equipo from '@/app/home/sections/Equipo';
-import FAQ from '@/app/home/sections/FAQ';
+// import FAQ from '@/app/home/sections/FAQ';
 
 export default function HomePage() {
-  const { t } = useTranslation();
   
   return (
     <>
@@ -33,14 +31,11 @@ export default function HomePage() {
       <Interseccion1 />
       <Productos />
       <Interseccion2 />
-      <Testimonios />
-      <Interseccion3 />
-      {/* <Equipo /> */}
       <Precios />
+      <Interseccion3 />
+      <Equipo />
       <Interseccion4 />
-      {/* <Herramientas /> */}
-      {/* <Interseccion5 /> */}
-      {/* <FAQ /> */}
+      <Testimonios />
   
     </>
   );
