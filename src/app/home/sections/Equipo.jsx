21e88489c031897e5efda596440c70fecd3681cf
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-10-11 03:07:35 +0200

    style: :lipstick: Equipo section updated

--- src/app/home/sections/Equipo.jsx
+++ src/app/home/sections/Equipo.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Section from '@/components/Section';
 import equipo from '@/json/equipo.json';
+import hectorImg from '@/assets/hector_img.jpg';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
+const IMAGES = {
+  hector_img: hectorImg,
+  // alvaro_img: alvaroImg,
+};
 
 export default function Equipo() {
   return (
@@ -9,16 +17,28 @@ export default function Equipo() {
         {equipo.map(item => (
           <li key={item.id}>
             <article className='equipo-card'>
-              <figure>
-                <img src='' alt='' />
-              </figure>
               <header>
-                <h3>{item.name} </h3>
-                <span className='label'>{item.label}</span>
+
+
+                  <Stack direction='row' spacing={2}>
+                    <Avatar
+                      alt='Remy Sharp'
+                    src={IMAGES[item.img]}
+                    sx={{ width: 75, height: 75 }}
+                  />
+                </Stack>
+
+                <div className='header-container'>
+                  <div className='title-container'>
+                    <h3>{item.name} </h3>
+                    <span className='label'>{item.label}</span>
+                  </div>
+                  <p>{item.rol}</p>
+                </div>
               </header>
-              <p>{item.rol}</p>
               <hr />
               <p>{item.description}</p>
+              <a href={item.link} target='_blank'>Conoce más...</a>
             </article>
           </li>
         ))}
